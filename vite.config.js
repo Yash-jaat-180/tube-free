ea0844e1a272d4a89f9ddb27b29a1ab7d6bb80ef
Yash <yashsaharan18024@gmail.com>
2025-04-23 14:10:31 +0530

    change the proxy url by the backend deployed url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:8000'
+      '/api': 'https://youtubebackend-0s8q.onrender.com'
     },
     host: true,
   },
